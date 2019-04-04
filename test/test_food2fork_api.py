ba75a09d9905526f5f961c78f82af4999516e7ca
Alex Richard Ford <arf4188@gmail.com>
2019-04-04 17:15:18 -0400

    improved variable names, improved comments

--- test/test_food2fork_api.py
+++ test/test_food2fork_api.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2
 
-"""Unit tests for the food2fork_api module."""
+"""Unit tests for the food2fork_api module.
+
+Author: Alex Richard Ford (arf4188@gmail.com)
+"""
 
 import pytest
 import colorlog
@@ -10,11 +13,14 @@ from food_finder import food2fork_api
 colorlog.basicConfig(level="INFO")
 _log = colorlog.getLogger(__name__)
 
+# Attempt to load the Food2Fork API Key from file, if it exists. Otherwise,
+# just set the API Key to some fake testing value.
 try:
     with open("./secrets/f2f_api_key", "r") as key_file:
         _KEY = key_file.readline().strip()
 except:
     _KEY = "fakeFood2ForkApiKey"
+
 
 def test_build_query_endpoint_url():
     api = food2fork_api.Food2ForkAPI(_KEY)
@@ -22,27 +28,29 @@ def test_build_query_endpoint_url():
     # Query endpoint requires a string type, so giving it None or some other
     # type should throw an exception
     with pytest.raises(AssertionError):
-        url = api._build_query_endpoint_url(None)
+        urlActual = api._build_query_endpoint_url(None)
     with pytest.raises(AssertionError):
-        url = api._build_query_endpoint_url(5)
+        urlActual = api._build_query_endpoint_url(5)
     
     # Subtest 2: query may be left empty, which defaults to q="", and is a valid
     # API request.
-    url = api._build_query_endpoint_url()
-    expected = "{}?key={}".format(food2fork_api._QUERY_ENDPOINT,
-                                  _KEY)
-    _log.info("Asserting that '{}' equals '{}'".format(url, expected))
-    assert url == expected
+    urlActual = api._build_query_endpoint_url()
+    urlExpected = "{}?key={}".format(food2fork_api._QUERY_ENDPOINT,
+                                     _KEY)
+    _log.info("Asserting that '{}' equals '{}'".format(urlActual, urlExpected))
+    assert urlActual == urlExpected
+
 
 def test_build_recipe_details_endpoint_url():
     api = food2fork_api.Food2ForkAPI(_KEY)
-    url = api._build_recipe_details_endpoint_url()
-    expected = "{}?key={}".format(food2fork_api._RECIPE_DETAILS_ENDPOINT,
-                                  _KEY)
-    _log.info("Asserting that '{}' equals '{}'".format(url, expected))
-    assert url == expected
+    urlActual = api._build_recipe_details_endpoint_url()
+    urlExpected = "{}?key={}".format(food2fork_api._RECIPE_DETAILS_ENDPOINT,
+                                     _KEY)
+    _log.info("Asserting that '{}' equals '{}'".format(urlActual, urlExpected))
+    assert urlActual == urlExpected
+
 
 def test_query_recipe():
+    # TODO: provide way to mock the API instead of actually doing it!
     api = food2fork_api.Food2ForkAPI(_KEY)
-    api.requests = None
     api.query_recipe(["vanilla", "butter", "cream cheese"])
