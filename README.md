6e13cced32a799efc7c9664c2be0fd13a94d0629
Alex Richard Ford <arf4188@gmail.com>
2019-04-05 12:38:36 -0400

    doc updates

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ See the [Take Home Assignment](./docs/take-home-assignment.md) page for the orig
   - [Development](#development)
   - [Testing](#testing)
 - [Sensitive Data](#sensitive-data)
+- [Unaddressed Considerations / Known Problems](#unaddressed-considerations--known-problems)
 
 # What You'll Need
 
@@ -71,3 +72,17 @@ See https://docs.pytest.org/en/latest/usage.html for additional information.
 The `./secrets` directory should be used to store local files containing any sensitive data. The entire `./secrets/` directory should be excluded from Git, so nothing in this directory should be committed to source control.
 
 The `./secrets/f2f_api_key` file may be used to specify your Food2Fork API key. This file is *optional*, but may be used by the app in different circumstances, if provided.
+
+# Unaddressed Considerations / Known Problems
+
+The following is a list of problems or nuances that apply to this application, which may be fixed in the future if desired:
+
+* The Free-tier of the Food2Fork API is limited to 50 requests per day, and the application has not been tested for when this happens.
+    * The code does nothing specific to handle this, and hasn't been tested when this case occurs. With that said, the code does check for a 200 (Success) HTTP status code and should enter a failure path assuming Food2Fork sets this code to something other than 200.
+
+* Sensitive data is allowed in the `./secrets/` directory, which is excluded from Git via the `.gitignore` file. This works, but could still make some folks uncomfortable because a slip-up while modifying `.gitignore` or failure to keep sensitive files isolated within `./secrets/` could leak sensitive data.
+    * A trivial refactoring could be done to move all sensitive files out of the applciation's project directory entirely, thus increasing the degree of separation between sensitive and non-sensitive files. (e.g. move `./secrets/` to `../food_finder_secrets/`)
+    * The data in `./secrets/` is not shared with the team in an defined way, at the moment. There are many ways to accomplish this, but one possible way is to archive and encrypt the sensitive files and actually commit the encrypted archive to Git. This is especially useful when sharing things like SSL certs and other non-user-specific sensitive files (i.e. not just a single API key file, which is specific to a user anyway). Then, the only thing that needs to be shared with team members is the password used to encrypt the sensitive data archive. (`git-crypt` and other tools can also be used to accomplish similar results.)
+
+  * At least one of the tests requires a valid API key and access to the actual Food2Fork API in order to pass.
+      * This makes automated testing more difficult. The API could be mocked in order to provide an isolated/offline environment for the test to run without the need for a real API key or the real Food2Fork API.
--- food_finder/food2fork_api.py
+++ food_finder/food2fork_api.py
@@ -19,7 +19,7 @@ _QUERY_ENDPOINT = "https://www.food2fork.com/api/search"
 _RECIPE_DETAILS_ENDPOINT = "https://www.food2fork.com/api/get"
 
 class Food2ForkAPI:
-    """Provides a light abstraction to the online Food2Fork API.
+    """Provides a light interface to the online Food2Fork API.
     
     Construct this using the desired API key and it will be used for all
     requests made by this instance.
@@ -28,31 +28,41 @@ class Food2ForkAPI:
     _log = None
 
     def __init__(self, api_key):
-        """Constructs a new Food2Fork API instance using the provided `api_key`."""
+        """
+        Constructs a new Food2Fork API instance using the provided `api_key`.
+        """
         self._api_key = api_key
         self._log = colorlog.getLogger("food_finder.{}".format(__name__))
 
 
     def _add_api_key_param(self, params):
-        """Private function for adding the 'api_key' param correctly."""
+        """
+        Private function for adding the 'api_key' param correctly.
+        """
         params['key'] = self._api_key
 
 
     def _add_query_param(self, query, params):
-        """Private function for adding the 'query' param correctly. The query
+        """
+        Private function for adding the 'query' param correctly. The query
         argument to this method should be a list of ingredient strings which
-        will be formatted appropriately for the Food2Fork API (comma delimited)."""
+        will be formatted appropriately for the Food2Fork API (comma delimited).
+        """
         assert type(query) is list, "`query` argument should be a list, but was: {}".format(query)
         params['q'] = ",".join(query)
 
 
     def _add_sort_param(self, sort, params):
-        """Private function for adding the 'sort' param correctly."""
+        """
+        Private function for adding the 'sort' param correctly.
+        """
         assert type(sort) is str
         params['sort'] = sort
     
     def _add_recipe_id_param(self, rId, params):
-        """Private function for adding the 'recipe ID' param correctly."""
+        """
+        Private function for adding the 'recipe ID' param correctly.
+        """
         assert (type(rId) is str) or (type(rId) is int) or (type(rId) is unicode), "Invalid type for 'rId', received type: {}".format(type(rId))
         params['rId'] = rId
     
@@ -61,11 +71,25 @@ class Food2ForkAPI:
                                  query=[""],
                                  sort="r",
                                  page=None):
-        """Builds a Query Endpoint URL for the Food2Fork API using the provided
-        parameter values. An empty query may be specified (as is the default)
-        which will simply return the current most popular recipes. The 'sort'
-        query parameter is set to default to 'r' which means receipes returned
-        will be sorted by social media rating."""
+        """
+        Private function which builds the URL for a query of the Food2Fork API.
+        
+        An empty `query` may be specified (as is the default) which will simply
+        return the current most popular recipes. The 'sort' query parameter is
+        set to default to 'r' which means receipes returned will be sorted by
+        social media rating.
+
+        Parameters:
+            query (list(str)):  a list of ingredients to search for.
+            
+            sort (char):        either 'r' or 't' to change how rating is done.
+            
+            page (int):         page number to get, where each page contains up
+                                to 30 recipe results.
+
+        Returns:
+            str:    a URL ready for use against the Food2Fork API.
+        """
         # initialize 'url' with the correct base URL
         url = _QUERY_ENDPOINT
         # create empty dict that we will then fill with our URL query parameters
@@ -80,6 +104,9 @@ class Food2ForkAPI:
 
     def _build_recipe_details_endpoint_url(self,
                                            recipe_id):
+        """
+        Private function which builds the URL for getting recipe details.
+        """
         # initialized 'url' with the correct base URL
         url = _RECIPE_DETAILS_ENDPOINT
         # start with empty dict, fill with needed query params
@@ -91,9 +118,20 @@ class Food2ForkAPI:
     
 
     def query_recipe(self, ingredients):
-        """Provides the most popular recipe that contains all of the specified
-        ingredients. The `ingredients` argument should be a list containing one
-        or more of the ingredients desired."""
+        """
+        Provides the most popular recipe containing all specified ingredients.
+        
+        Parameters:
+            ingredients (list(str)): A list containing one or more of the
+                                     ingredients desired.
+        
+        Returns:
+            Recipe (dict): A dictionary representing the Recipe attributes as
+                           given by the Food2Fork API, along with a 
+                           'missing_ingredients' attribute which is the
+                           difference between the specified `ingredients` and
+                           the ones needed by the recipe.
+        """
         url = self._build_query_endpoint_url(query=ingredients)
         self._log.debug("Built Query URL: {}".format(url))
         response = requests.get(url)
